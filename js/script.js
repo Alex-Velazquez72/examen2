0863817f9a2538e3147c433172847b0fab9c9c4a
Alex Velazquez72 <2302040@utrivieramaya.edu.mx>
2024-10-17 09:29:26 -0500

    Examen

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navItems = document.querySelectorAll('.nav-item');
+    navItems.forEach((item, index) => {
+        setTimeout(() => {
+            item.classList.add('show');
+        }, 100 * index);
+    })
+})
+
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// script de contacto
+
+const value = document.querySelector('#range_value')
+const input = document.querySelector('#range')
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+    value.textContent = event.target.value;
+});
